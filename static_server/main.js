dd1645b77fb338ee1fd82fa34a62744763b64af6
Mugundhan-strugglebreaker <srmvmugund@gmail.com>
2025-01-26 15:45:56 -0800

    move css change to main.js

--- static_server/main.js
+++ static_server/main.js
@@ -1,1 +1,5 @@
+import classes from "./style.module.css";
+console.log(classes)
+document.querySelector("title").className = classes.title
+document.querySelector("h1").className = classes.h1
 console.log("Hello world !!!")
